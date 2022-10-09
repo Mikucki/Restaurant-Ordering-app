3a0e14bc70590f92fc8f11c23086949803086e71
Mikucki <piotr.mikucki@op.pl>
2022-10-09 21:17:43 +0200

    You can add items now :D

--- src/data.js
+++ src/data.js
@@ -1,23 +1,25 @@
 const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2
-    }
-]
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+  },
+];
+
+export default menuArray;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log("hello");
+import data from "./data.js";
+
+const list = document.getElementById("list");
+const summery = document.getElementById("summery");
+
+function renderItem() {
+  for (let food of data) {
+    list.innerHTML += `<main class="item">
+        <div class="item-photo">${food.emoji}</div>
+        <div class="item-food">
+          <h2>${food.name}</h2>
+          <p>${food.ingredients}</p>
+          <h3>Price: ${food.price}$</h3>
+        </div>
+        <div class="item-add-to-cart">
+          <button class="item-order" data-cart="${food.id}">
+            <i id="icon-cart" class="fa-solid fa-cart-plus" data-cart="${food.id}"></i>
+          </button>
+        </div>
+      </main>
+      <div class="line"></div>`;
+  }
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.cart) {
+    
+  }
+});
+
+renderItem();
