4a5c9cd7cabdc1a2f169cff575f85caa9818f0ec
Mikucki <piotr.mikucki@op.pl>
2022-10-10 21:30:33 +0200

    hello

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import data from "./data.js";
 
 const list = document.getElementById("list");
 const summery = document.getElementById("summery");
+let orderArr = [];
 
 function renderItem() {
   for (let food of data) {
@@ -22,9 +23,20 @@ function renderItem() {
   }
 }
 
+function renderItemSummery() {
+  document.getElementById("appent-item").innerHTML += `
+  <div id="summery-item" class="summery-item">
+    <div class="summery-flex">
+      <h3>Pizza</h3>
+      <button class="remove-btn">remove</button>
+    </div>
+    <h3>${data.price}</h3>
+  </div>`;
+}
+
 document.addEventListener("click", function (e) {
   if (e.target.dataset.cart) {
-    
+    renderItemSummery();
   }
 });
 
